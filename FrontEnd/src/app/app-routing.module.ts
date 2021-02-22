d75452c1732480d068289b5a2210308c20f0b2cb
art3mis145 <art3mis145@gmail.com>
2021-02-22 13:22:29 +0700

    history

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'history', loadChildren: () => import('./pages/history/history.module').then(m => m.HistoryModule) }, { path: 'lobby', loadChildren: () => import('./pages/lobby/lobby.module').then(m => m.LobbyModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ FrontEnd/src/app/pages/history/history.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HistoryRoutingModule } from './history-routing.module';
+import { HistoryComponent } from './history.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [HistoryComponent],
+  imports: [
+    CommonModule,
+    HistoryRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+  ],
+})
+export class HistoryModule {}
