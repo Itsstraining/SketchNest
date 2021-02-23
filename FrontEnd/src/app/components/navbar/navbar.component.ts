635fed009ee5e76a761ec77d2439e5f54d6d2a20
gian <gian.tt3046@sinhvien.hoasen.edu.vn>
2021-02-23 10:25:16 -0800

    update navigate

--- FrontEnd/src/app/components/navbar/navbar.component.ts
+++ FrontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService, private afAuth: AngularFireAuth, public router:Router) { }
+  public user = this.auth.user;
 
   ngOnInit(): void {
-  }
 
+  }
+  async login() {
+    try {
+      await this.auth.oAuthLogin();
+      console.log("login success! "+this.auth.user.displayName );
+      await this.router.navigate([''])
+    }catch(err){
+      throw(err);
+    }
+    
+  }
+  async logout() {
+    await this.auth.logout();
+  }
+}
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
 }
