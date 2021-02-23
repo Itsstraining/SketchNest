ebdbb2b8d8e4ca66ee25bf65cb4c8125c0ef3516
gian <gian.tt3046@sinhvien.hoasen.edu.vn>
2021-02-22 17:49:59 -0800

    fix

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+  { path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
   {
     path: 'home',
     loadChildren: () =>
