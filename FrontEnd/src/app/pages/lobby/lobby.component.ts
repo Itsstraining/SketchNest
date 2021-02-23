5564a0c5ef63eefeb87b6df178ad52feca3afbd7
art3mis145 <art3mis145@gmail.com>
2021-02-23 15:33:07 +0700

    duy

--- FrontEnd/src/app/pages/lobby/lobby.component.ts
+++ FrontEnd/src/app/pages/lobby/lobby.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
-  styleUrls: ['./lobby.component.scss']
+  styleUrls: ['./lobby.component.scss'],
 })
 export class LobbyComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
