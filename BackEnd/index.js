8093830cd18440588e785eae8a2bcbbee0063580
gian <gian.tt3046@sinhvien.hoasen.edu.vn>
2021-02-24 10:31:13 -0800

    fixUI

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,16 +1,20 @@
 const express =require('express');
 const app=express();
 const Server=require("http").Server(app);
-const io= require('socket.io');
+const body = require('body-parser');
+// const io= require('socket.io');
+app.use(body,json());
+let array = {
+ name:"gian",
+ ip:"123123"
+}
 
-
-Server.listen(3000,()=>{
-    console.log("Server is running at port: "+3000);
-})
 app.get("/",(req,res)=>{
-    res.sendFile("");
+    res.send(array);
 })
-io.on('connection',function(){
-    console.log(" co nguoi dang nhap");
+// io.on('connection',function(){
+//     console.log(" co nguoi dang nhap");
+// })
+Server.listen(3000,"0.0.0.0",()=>{
+    console.log("Server is running");
 })
-
