bbb2a460b172eec7ee108759647377c87153a7b3
art3mis145 <art3mis145@gmail.com>
2021-02-25 10:18:33 +0700

    backend 2.0

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,20 +1,38 @@
+// const config = require("./config");
+// const app = require("express")();
+// const http = require("http").createServer(app);
+// const io = require("socket.io")(http, {
+//   cors: {
+//     origins: "*",
+//     method: ["GET","POST"],
+//   },
+// });
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hey Socket.io</h1>");
+// });
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
+// http.listen(config.PORT, config.HOST, () => {
+//   console.log("listening on *:3000");
+// });
+
 const config = require("./config");
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  cors: {
-    origins: ["http://localhost:4200"],
-  },
-});
+const io = require("socket.io")(http);
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hey Socket.io</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+  console.log("A user connected");
 });
-http.listen(config.PORT, config.HOST, () => {
-  console.log("listening on *:3000");
+
+http.listen(3000, "0.0.0.0", function () {
+  console.log("Server is running on port 3000");
 });
