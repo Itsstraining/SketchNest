7d9517b41cf498ea9dc2732e3a4384a4d5668f25
NguyenHoangTrung <trungdoublelift@gmail.com>
2021-02-26 17:11:19 +0700

    keypressbutton still bug

--- FrontEnd/src/app/pages/draw/draw.component.ts
+++ FrontEnd/src/app/pages/draw/draw.component.ts
@@ -12,7 +12,13 @@ import { Color } from 'fabric/fabric-impl';
   templateUrl: './draw.component.html',
   styleUrls: ['./draw.component.scss'],
 })
+
 export class DrawComponent implements OnInit, OnDestroy {
+  numberOfClicks
+  @HostListener('onkeydown', ['$event.target'])
+  onClick(btn) {
+    this.keyboardEvents();
+  }
   brush: any;
   canvas: any;
   circle: any;
@@ -22,6 +28,7 @@ export class DrawComponent implements OnInit, OnDestroy {
   currentMode: any;
   color: any;
   json: any;
+  triangle: any;
   constructor(public dialog: MatDialog) { }
   openDialog() {
     this.dialog.open(DialogExampleComponent);
@@ -31,13 +38,12 @@ export class DrawComponent implements OnInit, OnDestroy {
       width: 1500,
       height: 800,
     });
+    // this.keyboardEvents();
     //load canvas:
     // this.canvas.clear();
     // this.canvas.loadFromJSON(this.json, function () {
     //   this.canvas.renderAll();
     // });
-
-
     //xac dinh vi tri con chuot trong canvas
     // this.canvas.on('mouse:move', function (event) {
     //   console.log(event.e.clientX, event.e.clientY);
@@ -47,7 +53,7 @@ export class DrawComponent implements OnInit, OnDestroy {
 
   }
   ngOnDestroy() {
-
+    //xuat canva thanh JSON
     this.json = JSON.stringify(this.canvas.toJSON());
   }
   //default
@@ -67,13 +73,16 @@ export class DrawComponent implements OnInit, OnDestroy {
     fabric.Path.prototype.selectable = false;
 
 
+
   }
   eraser() {
     this.canvas.isDrawingMode = true;
     this.canvas.freeDrawingBrush.color = 'white';
     this.canvas.freeDrawingBrush.width = 14;
     fabric.Path.prototype.selectable = false;
+    console.log(this.json);
   }
+
   url: any;
   picture(event) {
     if (event.target.files && event.target.files[0]) {
@@ -87,14 +96,32 @@ export class DrawComponent implements OnInit, OnDestroy {
           this.canvas.renderAll();
         })
       }
-     
-
-
     }
-
   }
-  // /Shape
+  ///Keyboard events
+  keyboardEvents() {
+    this.canvas.document.onkeydown = function (e) {
+      switch (e.keyCode) {
+        case 46://xoa
+          alert('deleted')
+          // if (this.canvas.getActiveObject()) {
+          this.canvas.getActiveObject().remove();
 
+          break;
+
+      }
+    }
+  }
+
+
+  // /Shape
+  deleteShape() {
+    this.canvas.isDrawingMode = false;
+    this.canvas.remove(this.canvas.getActiveObject());
+
+
+    console.log(this.json);
+  }
   drawCircle() {
     this.canvas.isDrawingMode = false;
     this.circle = new fabric.Circle({
@@ -130,13 +157,7 @@ export class DrawComponent implements OnInit, OnDestroy {
 
     ///////////////////////
   }
-  deleteShape() {
-    this.canvas.isDrawingMode = false;
-    this.canvas.remove(this.canvas.getActiveObject());
-    this.json = JSON.stringify(this.canvas.toJSON());
-    console.log(this.json);
 
-  }
 
 
 
@@ -150,6 +171,16 @@ export class DrawComponent implements OnInit, OnDestroy {
     this.canvas.add(this.rect);
     this.canvas.renderAll();
   }
+  drawTriangle() {
+    this.canvas.isDrawingMode = false;
+    this.triangle = new fabric.Triangle({
+      width: 100,
+      height: 100,
+      fill: 'blue',
+    })
+
+  }
+
   //ShapeOption
   shapeOption() {
 
